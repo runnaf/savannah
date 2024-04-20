6bd6338b473982fd1612dc8e32838ddb8201a75b
GozeFullStack <125929344+GozelFullStackDev@users.noreply.github.com>
2024-04-19 20:38:38 -0700

    Add files via upload

---
+++ react-fsddesignkitty/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+
+const App = () => {
+  return (
+    <Router> 
+      <div>
+        <h1>My Cat App</h1>
+      {/* <CatList/> */}
+      </div>
+    </Router> 
+  );
+};
+
+export default App;
---
+++ react-fsddesignkitty/src/features/Advice/AdviceList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AdviceList = () => {
+  return (
+    <div>AdviceList</div>
+  )
+}
+
+export default AdviceList
