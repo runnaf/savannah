0e4f2312421420e124c44fa20c5cfacbaeb24a3b
tamara-danilova21 <sap1028@mail.ru>
2024-05-20 11:50:30 -0400

    fix size text

--- src/entities/AboutUs/AboutUs.js
+++ src/entities/AboutUs/AboutUs.js
@@ -10,7 +10,7 @@ const AboutUs = () => {
         <div>
             <section className={styles.section}>                    
                 <Stack>
-                    <Text type="h2" size="xl" className={styles.title}>
+                    <Text type="h2" size="l" className={styles.title}>
                         О ПИТОМНИКЕ <strong>SAVANNAH WORLD</strong>
                     </Text>
                 </Stack>
@@ -23,13 +23,13 @@ const AboutUs = () => {
                             direction = 'column'
                             alignItems='alignStart'
                             className={styles.text}>
-                                <Text size="l" className={styles.par}>
+                                <Text size="m" className={styles.par}>
                                     Нас завораживает красота и грация Саванн! 
                                 </Text>
-                                <Text size="l" className={styles.par}>
+                                <Text size="m" className={styles.par}>
                                     Питомник Savanna World, в лице заводчика Светланы Костроминой зарегистрирован в TICA и обладает всеми необходимыми документами.
                                 </Text>
-                                <Text size="l" className={styles.par}>
+                                <Text size="m" className={styles.par}>
                                     Мы стремимся к получению котят прекрасным внешним видом и отличным здоровьем. Поэтому на регулярной основе проводим анализы и осмотры ветеринарным врачом.
                                 </Text>                                  
                         </Stack>
@@ -38,7 +38,7 @@ const AboutUs = () => {
             </section>
             <section className={styles.section}>
                 <Stack>
-                    <Text type="h2" size="xl" className={styles.title}>
+                    <Text type="h2" size="l" className={styles.title}>
                         ПИТОМНИК <strong>SAVANNAH WORLD</strong> ЭТО -
                     </Text>
                 </Stack>
@@ -49,17 +49,17 @@ const AboutUs = () => {
                         alignItems='alignStart'
                         className={styles.benefits__row}>
                         <div className={styles.benefit__doc}>                        
-                            <Text type="h3" size="m" className={styles.benefit__text}>
+                            <Text type="h3" size="s" className={styles.benefit__text}>
                                 Документация согласно стандартам породы
                             </Text>
                         </div>
                         <div className={styles.benefit__vet}> 
-                            <Text type="h3" size="m" className={styles.benefit__text}>
+                            <Text type="h3" size="s" className={styles.benefit__text}>
                                 Полное ветеринарное обследование и вакцинация
                             </Text>
                         </div>
                         <div className={styles.benefit__question}> 
-                            <Text type="h3" size="m" className={styles.benefit__text}>
+                            <Text type="h3" size="s" className={styles.benefit__text}>
                                 Консультация по вопросам содержания и воспитания
                             </Text>
                         </div>
