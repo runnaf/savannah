4a94021d03981ed1b016bbfd14af287faccf4e95
“Irina <runnaf@mail.ru>
2024-05-29 12:43:41 +0300

    added hook useAppRoutes

---
+++ src/shared/hooks/useAppRoutes.js
@@ -0,0 +1,14 @@
+import { useMemo } from "react";
+
+export const useAppRoutes = (props) => {
+    const {routeConfig, includeIcons = false} = props
+    return useMemo(() => {
+    return Object.values(routeConfig)
+        .filter(route => !route.adminOnly)
+        .map(route => ({
+            path: route.path,
+            navlink: route.navlink,
+            icon: includeIcons ? route.icon : null
+        }));
+    }, [routeConfig, includeIcons]);
+};
