21a0df8fe59332fe9d02b1b9ca787168657e70c7
veruzhskaya <veruzhskaya@gmail.com>
2024-05-29 19:48:54 +0300

    our kittens

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,10 +1,12 @@
 import AboutUs from '../../entities/AboutUs/AboutUs';
+import OurKittens from '../../widgets/OurKittens/OurKittens';
 import styles from './MainPage.module.scss';
 
 export const MainPage = () => {
   return (
     <main className={styles.MainPage}>
       <AboutUs />
+      <OurKittens/>
     </main>
   )
 };
---
+++ src/widgets/OurKittens/OurKittens.js
@@ -0,0 +1,72 @@
+import { Button } from '../../shared/ui/Button/Button'
+import { Stack } from '../../shared/ui/Stack/Stack';
+import { Text } from "../../shared/ui/Text/Text";
+import styles from './OruKittens.module.scss';
+import kittenImg from '../../shared/assets/photo/kitten-1x.png';
+import { arrowIcon } from '../../shared/assets/svg/arrowIcon';
+
+
+const OurKittens = () => {
+    return (
+        <div>
+            <section className={styles.section}>                    
+                <Stack>
+                    <Text type="h2" size="xl" className={styles.title}>
+                    НАШИ КОТЯТА
+                    </Text>
+                </Stack>
+
+                <div className={styles.container}>                
+                    <div className={styles.kittens__row}>
+                        <div className={styles.card__item__one}>
+                            <article className={styles.kittens__card}>
+                                    <div className={styles.card__image}>
+                                        <img className={styles.kitten__img} src={kittenImg} alt='котята Саванны'/>
+                                    </div>  
+                                    <div className={styles.card__body}>
+                                        <Text size="l" className={styles.kitten__name}>Имя</Text>                                                                 
+                                        <Text  size="m" className={styles.description}>
+                                            Описание Описание Описание Описание Описание Описание
+                                        </Text>                     
+                                        <Button className={styles.kitten__btn}>В каталог {arrowIcon()}</Button>
+                                    </div>                            
+                            </article> 
+                        </div>
+                            
+                        <div className={styles.card__item__two}>
+                            <article className={styles.kittens__card}>
+                                <div className={styles.card__image}>
+                                    <img className={styles.kitten__img} src={kittenImg} alt='котята Саванны'/>
+                                </div>  
+                                <div className={styles.card__body}>
+                                    <Text size="l" className={styles.kitten__name}>Имя</Text>                                                                 
+                                    <Text  size="m" className={styles.description}>
+                                        Описание Описание Описание Описание 
+                                    </Text>                     
+                                    <Button className={styles.kitten__btn}>В каталог {arrowIcon()}</Button>
+                                </div>                             
+                            </article> 
+                        </div>
+                        <div className={styles.card__item__three}>
+                            <article className={styles.kittens__card}>
+                                <div className={styles.card__image}>
+                                    <img className={styles.kitten__img} src={kittenImg} alt='котята Саванны'/>
+                                </div>  
+                                
+                                <div className={styles.card__body}>
+                                    <Text size="l" className={styles.kitten__name}>Имя</Text>                                                                 
+                                    <Text  size="m" className={styles.description}>
+                                        Описание Описание 
+                                    </Text>                     
+                                    <Button className={styles.kitten__btn}>В каталог {arrowIcon()}</Button>
+                                </div>                                                             
+                            </article> 
+                        </div>
+                    </div>               
+                </div>    
+            </section>
+        </div>
+      );
+}
+ 
+export default OurKittens;
