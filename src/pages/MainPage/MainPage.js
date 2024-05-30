b1ffb0402d3640f250e7c5a9496584313034c225
tamara_danilova <roznica14@yandex.ru>
2024-05-30 10:49:33 +0400

    upd ourKittens

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,5 +1,5 @@
 import AboutUs from '../../entities/AboutUs/AboutUs';
-import OurKittens from '../../widgets/OurKittens/OurKittens';
+import OurKittens from '../../entities/OurKittens/ui/OurKittensList/OurKittens';
 import AccordionComponent from '../../entities/AccordionComponent/ui/AccordionComponent';
 import styles from './MainPage.module.scss';
 
