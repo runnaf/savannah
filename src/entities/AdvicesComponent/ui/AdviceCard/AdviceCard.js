c6eec1a577b5e00added66eecbf0c57477596ff7
Kate Danilova <ecdanilova@gmail.com>
2024-06-01 12:20:45 -0400

    added advices section

---
+++ src/entities/AdvicesComponent/ui/AdviceCard/AdviceCard.js
@@ -0,0 +1,25 @@
+import styles from "./AdviceCard.module.scss";
+import { Text } from "../../../../shared/ui/Text/Text";
+// import { adviceData } from "../../lib/adviceData";
+
+const AdviceCard = ({cat, title, content}) => {
+  return (
+    <div className={styles.container}>
+      <div>
+        <img src={cat} className={styles.img}  alt="Cat" />
+      </div>
+      <div>
+        <Text className={styles.title} type="h2">
+          {title}
+        </Text>
+        <Text className={styles.content} type="p">
+          {content}
+        </Text>
+      </div>
+    </div>
+  );
+};
+
+export default AdviceCard;
+
+
--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -2,6 +2,7 @@ import AboutUs from '../../entities/AboutUs/AboutUs';
 import { BreedCharacteristics } from '../../entities/BreedCharacteristics/ui/BreedCharacteristics/BreedCharacteristics';
 import OurKittens from '../../entities/OurKittens/ui/OurKittensList/OurKittens';
 import AccordionComponent from '../../entities/AccordionComponent/ui/AccordionComponent';
+import { Advices } from '../../entities/AdvicesComponent/ui/Advices/Advices'
 import styles from './MainPage.module.scss';
 
 export const MainPage = () => {
@@ -10,6 +11,7 @@ export const MainPage = () => {
       <AboutUs />
       <OurKittens/>
       <BreedCharacteristics />
+      <Advices/>
       <AccordionComponent />
     </main>
   )
