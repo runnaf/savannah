ee792536dd02907f5cba571c630254bb3660bf68
“Irina <runnaf@mail.ru>
2024-06-07 15:10:05 +0300

    create header-height mixin

--- src/app/App.js
+++ src/app/App.js
@@ -1,3 +1,4 @@
+import { useLocation } from 'react-router';
 import ButtonSocial from '../widgets/ButtonSocial/ButtonSocial';
 import { ButtonUp } from '../shared/ui/ButtonUp/ButtonUp';
 import { RouterProvider } from './providers/router/RouterProvider/RouterProvider';
@@ -5,10 +6,12 @@ import { Header } from '../widgets/Header/ui/Header/Header';
 import './styles/index.scss';
 import styles from './App.module.scss';
 
+
 function App() {
+  const location = useLocation();
 
   return (
-    <div className={styles.container}>
+    <div className={`${styles.container} ${location.pathname === '/'? styles.containerMain : ''}`}>
       <Header />
       <RouterProvider />
       <ButtonUp />
