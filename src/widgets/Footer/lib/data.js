e8cc849ae47e0f32c9d47a790ee0f91a94b6dc9f
Nataliya Khromykh <nataliyakhromykh@Nataliyas-MacBook-Pro.local>
2024-06-29 00:33:45 -0400

    adjustments for tablet and mobile

--- src/widgets/Footer/lib/data.js
+++ src/widgets/Footer/lib/data.js
@@ -1,4 +1,4 @@
-import styles from "../ui/Footer.module.scss";
+
 import phoneIcon from "../../../shared/assets/footer/phone.svg";
 import envelopeIcon from "../../../shared/assets/footer/mail.svg";
 import placemarkerIcon from "../../../shared/assets/footer/map.svg";
@@ -7,50 +7,49 @@ import instaIcon from "../../../shared/assets/footer/instagram.svg";
 import vkIcon from "../../../shared/assets/footer/vk.svg";
 
 
-export const sectionData = [
+export const contacts = [
     {
         id: 1,
         icon: phoneIcon,
         alt: 'телефон',
-        hiddenText: 'номер телефона',
-        content: '+999 888-77-66'
+        href: 'tel:+9998887766',
+        text: '+999 888-77-66'
     },
     {
         id: 2,
         icon: envelopeIcon,
         alt: 'эмейл',
-        hiddenText: 'эмейл',
-        content: <a className={styles.email} target='_blank' rel="noreferrer" href="mailto:savannahworld@gmail.com">savannahworld@gmail.com</a>
+        href: 'mailto:savannahworld@gmail.com',
+        text: 'savannahworld@gmail.com'
     },
     {
         id: 3,
         icon: placemarkerIcon,
         alt: 'адрес',
-        hiddenText: 'адрес',
-        content: 'г.Москва, ул.Отрадная, дом 1'
+        text: 'г.Москва, ул.Отрадная, дом 1'
     }
 ];
 
-export const mediaData =[
+export const mediaData = [
     {
         id: 1,
         icon: telegramIcon,
         alt: 'телеграм',
-        hiddenText: 'телеграм',
-        content: <a className={styles.email} target='_blank' rel="noreferrer" href="https://t.me/savannahworld">@savannahworld</a>
+        href: 'https://t.me/savannahworld',
+        text: '@savannahworld'
     },
     {
         id: 2,
         icon: instaIcon,
         alt: 'инстаграм',
-        hiddenText: 'инстаграм',
-        content: <a className={styles.email} target='_blank' rel="noreferrer" href="https://instagram.com/savannahworld">@savannahworld</a>
+        href: 'https://instagram.com/savannahworld',
+        text: '@savannahworld'
     },
     {
         id: 3,
         icon: vkIcon,
         alt: 'вконтакте',
-        hiddenText: 'вконтакте',
-        content: <a className={styles.email} target='_blank' rel="noreferrer" href="https://vk.com/savannahworld">@savannahworld</a>
+        href: 'https://vk.com/savannahworld',
+        text: '@savannahworld'
     }
-]
+];
