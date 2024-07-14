869ede86c41ffdb02aea945cfa3ff556c06a7c86
tamara-danilova2105 <sap1028@mail.ru>
2024-07-14 19:19:22 +0400

    added crud

--- src/pages/CatalogPage/ui/CatalogPage.js
+++ src/pages/CatalogPage/ui/CatalogPage.js
@@ -6,9 +6,24 @@ import { useGetCatsQuery } from "../api/api";
 
 
 export const CatalogPage = () => {
-
     ScrollToTop();
-    const { data: cats, error, isLoading } = useGetCatsQuery({});
+
+    const filterParams = {
+        group: [],
+        sex: ['самка'],
+        age: ['котята'],
+        shipment: []
+    }; //TEST - из REDUX потом брать 
+
+    const params = {
+        generate: filterParams.group?.join(','),
+        sex: filterParams.sex?.join(','),
+        age: filterParams.age?.join(','),
+        shipment: filterParams.shipment?.join(',')
+    };
+
+    const { data: cats, error, isLoading } = useGetCatsQuery(params);
+    console.log(cats);
 
     return (
         <main className={styles.main}>
