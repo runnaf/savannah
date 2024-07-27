7019f61712865062e953047c636e0a24d6d2a47f
tamara-danilova2105 <sap1028@mail.ru>
2024-07-27 08:24:26 +0400

    fix

--- src/shared/ui/UploadImage/UploadImage.js
+++ src/shared/ui/UploadImage/UploadImage.js
@@ -1,14 +1,11 @@
 import styles from './UploadImage.module.scss';
 import { useModal } from '../../hooks/useModal';
 import { CropModal } from '../CropModal/CropModal';
-import editIcon  from '../../assets/photo/editPhotoIcon.png'
+import editIcon from '../../assets/photo/editPhotoIcon.png'
 import { useRef } from 'react';
 import emptyCard from '../../assets/photo/emptyCard.png'
 
-
-
-
-export const UploadImage = ({uploadFileFromDisk, imagePreview})=> {
+export const UploadImage = ({ uploadFileFromDisk, imagePreview }) => {
     const [changeCropModal, drawCropModal] = useModal();
     const catUrl = useRef(emptyCard)
     const updateCatCard = (imagePreview) => {
@@ -17,18 +14,18 @@ export const UploadImage = ({uploadFileFromDisk, imagePreview})=> {
 
     return (
         <div className={styles.upload_container}>
-            <img src={catUrl.current} className={styles.imgContainer} alt=''/>
-        {drawCropModal(
-            <CropModal changeCropModal = {changeCropModal}
-                        updateCatCard={updateCatCard}  
-                        uploadFileFromDisk={uploadFileFromDisk}
-                        imagePreview ={imagePreview}                                         
-                      />
-                   
-        )}
-              <img className={styles.editIcon}
-               onClick={changeCropModal}
-            src={editIcon} alt="editIcon" />
+            <img src={catUrl.current} className={styles.imgContainer} alt='' />
+            {drawCropModal(
+                <CropModal changeCropModal={changeCropModal}
+                    updateCatCard={updateCatCard}
+                    uploadFileFromDisk={uploadFileFromDisk}
+                    imagePreview={imagePreview}
+                />
+
+            )}
+            <img className={styles.editIcon}
+                onClick={changeCropModal}
+                src={editIcon} alt="editIcon" />
         </div>
     );
 };
--- src/widgets/CreateCatCard/CreateCatCard.js
+++ src/widgets/CreateCatCard/CreateCatCard.js
@@ -6,7 +6,7 @@ import { Text } from "../../shared/ui/Text/Text";
 import { EditAddForm } from "../../feature/EditAddForm/ui/EditAddForm/EditAddForm";
 import { Button } from "../../shared/ui/Button/Button";
 import { arrowIcon } from "../../shared/assets/svg/arrowIcon";
-import {  initCatCard, resetCatCard, getCatCard } from "../../feature/EditAddForm/model/Slice";
+import { initCatCard, resetCatCard, getCatCard } from "../../feature/EditAddForm/model/Slice";
 import { useSaveCatMutation, useUploadFileMutation } from '../../pages/CatalogPage/api/api';
 import { useDispatch, useSelector } from "react-redux";
 import { useCallback, useEffect, useState } from "react";
@@ -14,29 +14,27 @@ import { UploadImage } from "../../shared/ui/UploadImage/UploadImage";
 
 
 
-export const CreateCatCard = ({ changeCreateModal }) => { 
+export const CreateCatCard = ({ changeCreateModal }) => {
 
     const dispatch = useDispatch();
-    const cat = useSelector(getCatCard);  
-    const [file, setFile] = useState();  
+    const cat = useSelector(getCatCard);
+    const [file, setFile] = useState();
     const [imagePreview, setImagePreview] = useState('');
     const [disabled, setDisabled] = useState(true);
-    const [statusReq, setStatusReq] = useState({ text: '', isError: false });   
+    const [statusReq, setStatusReq] = useState({ text: '', isError: false });
     const [saveCat, { isLoading: isSaving }] = useSaveCatMutation();
     const [uploadFile, { isLoading: isUploading }] = useUploadFileMutation();
     const isLoading = isSaving || isUploading;
 
 
-    
-    const uploadFileFromDisk = async (e) => {         
+
+    const uploadFileFromDisk = async (e) => {
         if (e.target.files && e.target.files.length > 0) {
             const file = e.target.files[0];
             setFile(file);
 
-         
             const fileUrl = URL.createObjectURL(file);
 
-                     
             setImagePreview(fileUrl);
         } else {
             setImagePreview(null);
@@ -44,28 +42,25 @@ export const CreateCatCard = ({ changeCreateModal }) => {
     };
 
     useEffect(() => {
-        const isEmpty = Object.values({cat}).every(value => value !== '');        
+        const isEmpty = Object.values({ cat }).every(value => value !== '');
         setDisabled(!isEmpty || !file);
     }, [cat, file]);
-    
-
-  
 
     const setFormData = useCallback((key, value) => {
-        dispatch( initCatCard({ key, value }))
+        dispatch(initCatCard({ key, value }))
     }, [dispatch]);
 
     const resetFormData = () => {
         changeCreateModal();
         dispatch(resetCatCard());
     };
-   
+
 
     const handleSaveCat = async () => {
         try {
             const fileResponse = await uploadFile(file).unwrap();
             const updatedCat = {
-                ...{cat},
+                ...{ cat },
                 images: fileResponse.url.split('/')[2]
             };
             await saveCat(updatedCat).unwrap();
@@ -88,11 +83,11 @@ export const CreateCatCard = ({ changeCreateModal }) => {
             direction="column"
             gap='32'
             className={styles.edit}
-        >       
+        >
             <div>
                 <img
                     className={styles.closeButton}
-                    src={closeButton} alt="закрыть"                  
+                    src={closeButton} alt="закрыть"
                     onClick={resetFormData}
                 />
             </div>
@@ -100,32 +95,32 @@ export const CreateCatCard = ({ changeCreateModal }) => {
             <Text type='h3' size='l' className={styles.title}>
                 Создать карточку питомца
             </Text>
-            <Stack 
-                direction = 'row'
-                justifyContent= 'justifyCenter'
-                alignItems = 'alignStart'
-                gap = '32'
+            <Stack
+                direction='row'
+                justifyContent='justifyCenter'
+                alignItems='alignStart'
+                gap='32'
                 className={styles.editSection}>
-                <UploadImage     
+                <UploadImage
                     uploadFileFromDisk={uploadFileFromDisk}
-                    imagePreview ={imagePreview}                    
+                    imagePreview={imagePreview}
                 />
-                <EditAddForm setForm={setFormData} />               
+                <EditAddForm setForm={setFormData} />
             </Stack>
             <div className={styles.save__btn}>
-                <Button 
-                className={styles.button}
-                disabled={disabled}
-                onClick={handleSaveCat}
+                <Button
+                    className={styles.button}
+                    disabled={disabled}
+                    onClick={handleSaveCat}
                 >
-                    сохранить 
-                        {isLoading
-                            ? <span className={styles.loader} />
-                            : <>{arrowIcon()}</>
-                        }
+                    сохранить
+                    {isLoading
+                        ? <span className={styles.loader} />
+                        : <>{arrowIcon()}</>
+                    }
                 </Button>
                 <Text className={`${styles.text} ${statusReq.isError ? styles.error : styles.default}`}>
-                        {statusReq.text}
+                    {statusReq.text}
                 </Text>
             </div>
         </Stack>
